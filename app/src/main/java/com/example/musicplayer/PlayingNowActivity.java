6d4e47bf907062463c83b79d867a0a0c52590500
MinhHoang2603 <minhhoang.2007bt@gmail.com>
2026-02-27 15:25:11 +0700

    playing layout

--- app/src/main/java/com/example/musicplayer/PlayingNowActivity.java
+++ app/src/main/java/com/example/musicplayer/PlayingNowActivity.java
@@ -1,6 +1,6 @@
 package com.example.musicplayer;
 
-import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,22 +9,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class PlayingNowActivity extends AppCompatActivity {
+    private MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_playing_now);
 
-        Intent intent = new Intent(this, PlayingNowActivity.class);
-        startActivity(intent);
-        finish();
+        bindViews();
+        setEvents();
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.playing_now_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    void bindViews() {
+
+    }
+    void setEvents() {
+
     }
 }
